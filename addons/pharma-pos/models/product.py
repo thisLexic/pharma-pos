70d3742d012ebf2bf9777b1c85edab95505dcd36
thislexic <benvillabroza1999@gmail.com>
2021-01-25 20:57:30 +0800

    made the name attribute unique in the Product_Name model

--- addons/pharma-pos/models/product.py
+++ addons/pharma-pos/models/product.py
@@ -4,5 +4,8 @@ from odoo import models, fields, api
 class Product_Name(models.Model):
     _name = 'pharma_pos.product_name'
     _description = 'Contains names of products'
+    _sql_constraints = [
+        ('name_unique', 'unique(name)', 'Cannot have duplicate product names!')
+    ]
 
     name = fields.Char(string="Product Name")
