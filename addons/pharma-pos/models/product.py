ab3da0ec93203a387848f150e6ba89bd35ddc341
thislexic <benvillabroza1999@gmail.com>
2021-01-25 21:30:02 +0800

    added the Product_Size model

--- addons/pharma-pos/models/product.py
+++ addons/pharma-pos/models/product.py
@@ -7,8 +7,19 @@ class Product_Name(models.Model):
     _name = 'pharma_pos.product_name'
     _description = 'Contains names of products'
     _sql_constraints = [
-        ('name_unique', 'unique(name)', 'Cannot have duplicate product names!')
+        ('product_name_name_unique', 'unique(name)', 'This product name already exists!')
     ]
 
     name = fields.Char(string="Product Name")
+    date_added = fields.Date(string="Date Added", default=date.today())
+
+
+class Product_Size(models.Model):
+    _name = 'pharma_pos.product_size'
+    _description = 'Contains sizes of products'
+    _sql_constraints = [
+        ('product_size_size_unique', 'unique(size)', 'This product size already exists!')
+    ]
+
+    size = fields.Char(string="Product Size")
     date_added = fields.Date(string="Date Added", default=date.today())
