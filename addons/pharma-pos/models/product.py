3e3c39467c8b33941c4874fb5901d460f9304bff
thislexic <benvillabroza1999@gmail.com>
2021-01-27 11:21:30 +0800

    added better title in Product_Size and Product_Name. Changed the string representation of their records for better UI

--- addons/pharma-pos/models/product.py
+++ addons/pharma-pos/models/product.py
@@ -10,6 +10,13 @@ class Product_Name(models.Model):
         ('product_name_generic_name_branded_name_unique', 'unique(generic_name, branded_name)', 'This product name already exists!')
     ]
 
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.generic_name + " ({})".format(record.branded_name)
+            result.append((record.id, name))
+        return result
+
     generic_name = fields.Char(string="Generic Name")
     branded_name = fields.Char(string="Branded Name")
     date_added = fields.Date(string="Date Added", default=date.today())
@@ -21,6 +28,13 @@ class Product_Size(models.Model):
     _sql_constraints = [
         ('product_size_size_unique', 'unique(size)', 'This product size already exists!')
     ]
+
+    def name_get(self):
+        result = []
+        for record in self:
+            name = record.size + " {}".format(record.consumption_method)
+            result.append((record.id, name))
+        return result
 
     size = fields.Char(string="Product Size")
     date_added = fields.Date(string="Date Added", default=date.today())
