92c16a21efb518181040fdea82045c93ef5c87e3
thislexic <benvillabroza1999@gmail.com>
2021-01-31 21:51:30 +0800

    cleaned up code for unique together constaint and string representation of Product_Name, Product_Type, and Product. Also added and debugged the excel sheets

--- addons/pharma-pos/models/product.py
+++ addons/pharma-pos/models/product.py
@@ -8,7 +8,7 @@ class Product_Name(models.Model):
     _description = 'Contains names of products'
     _rec_name ='string_rep'
     _sql_constraints = [
-        ('product_name_generic_name_branded_name_unique', 'unique(generic_name, branded_name)', 'This product name already exists!')
+        ('product_name_generic_name_branded_name_unique', 'unique(string_rep)', 'This product name already exists!')
     ]
 
     @api.depends('generic_name', 'branded_name')
@@ -49,34 +49,22 @@ class Product_Size(models.Model):
 class Product_Type(models.Model):
     _name = 'pharma_pos.product_type'
     _description = 'Contains types of products'
+    _rec_name ='consumption_method'
     _sql_constraints = [
         ('product_type_consumption_method_unique', 'unique(consumption_method)', 'This product type already exists!')
     ]
-
-    def name_get(self):
-        result = []
-        for record in self:
-            name = record.consumption_method
-            result.append((record.id, name))
-        return result
 
     consumption_method = fields.Char(string="Type")
 
 class Product(models.Model):
     _name = 'pharma_pos.product'
     _description = 'The product that is sold with its name (Product_Name) and unit (Product_Size) specified using foreign keys'
+    _rec_name ='string_rep'
     _sql_constraints = [
         ('product_store_code_unique', 'unique(store_code)', 'This store code already exists!'),
         ('product_supplier_code_unique', 'unique(supplier_code)', 'This supplier code already exists!'),
-        ('product_product_name_id_product_size_id_unique', 'unique(product_name_id, product_size_id)', 'This combination of product name and size already exists!')
+        ('product_product_name_id_product_size_id_unique', 'unique(string_rep)', 'This combination of product name and size already exists!')
     ]
-
-    def name_get(self):
-        result = []
-        for record in self:
-            name = record.string_rep
-            result.append((record.id, name))
-        return result
 
     @api.depends('product_name_id', 'product_size_id')
     def _get_string_rep(self):
