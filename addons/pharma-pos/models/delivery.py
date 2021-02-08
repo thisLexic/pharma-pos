fa8775c8220681ef27a8e6840d49f53c3464e193
thislexic <benvillabroza1999@gmail.com>
2021-02-08 14:46:37 +0800

    add filter to price_id FK to only display is_sold is True

--- addons/pharma-pos/models/delivery.py
+++ addons/pharma-pos/models/delivery.py
@@ -4,4 +4,5 @@ class Batch(models.Model):
     _name = 'pharma_pos.batch'
     _description = 'Contains a batch of packs with the same prices'
 
-    price_id = fields.Many2one('pharma_pos.price', string="Product")
+    price_id = fields.Many2one('pharma_pos.price', string="Product", domain="[('is_sold', '=', True)]")
+    
