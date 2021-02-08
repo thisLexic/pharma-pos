83fc0cc3188f72bf44f0f9606250698c52843099
thislexic <benvillabroza1999@gmail.com>
2021-02-08 14:57:05 +0800

    added more attributes to Batch model

--- addons/pharma-pos/models/delivery.py
+++ addons/pharma-pos/models/delivery.py
@@ -5,4 +5,8 @@ class Batch(models.Model):
     _description = 'Contains a batch of packs with the same prices'
 
     price_id = fields.Many2one('pharma_pos.price', string="Product", domain="[('is_sold', '=', True)]")
-    
+    batch_number = fields.Char(string="Batch Number")
+    bought_count = fields.Integer(string="Packs Bought")
+    sold_count = fields.Integer(string="Packs Sold")
+    left_count = fields.Integer(string="Packs Left")
+    expiration_date = fields.Date(string="Expiration Date")
