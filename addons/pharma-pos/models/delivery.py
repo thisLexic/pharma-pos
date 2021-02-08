636c20b6e93ad750fa90c47f165bef8532cc29af
thislexic <benvillabroza1999@gmail.com>
2021-02-08 15:29:49 +0800

    added string representation for Batch record

--- addons/pharma-pos/models/delivery.py
+++ addons/pharma-pos/models/delivery.py
@@ -1,8 +1,19 @@
 from odoo import models, fields, api
 
+from .tools import getStringRepresentation
+
 class Batch(models.Model):
     _name = 'pharma_pos.batch'
     _description = 'Contains a batch of packs with the same prices'
+    _rec_name ='string_rep'
+
+    @api.depends('price_id.string_rep', 'expiration_date')
+    def _get_string_rep(self):
+        for record in self:
+            if record.price_id and record.expiration_date:
+                record.string_rep = getStringRepresentation(record.price_id) + " ({})".format(record.expiration_date)
+            else:
+                record.string_rep = "No Value"
 
     price_id = fields.Many2one('pharma_pos.price', string="Product", domain="[('is_sold', '=', True)]")
     batch_number = fields.Char(string="Batch Number")
@@ -10,3 +21,4 @@ class Batch(models.Model):
     sold_count = fields.Integer(string="Packs Sold")
     left_count = fields.Integer(string="Packs Left")
     expiration_date = fields.Date(string="Expiration Date")
+    string_rep = fields.Char(string="Name", compute="_get_string_rep", store=True)
