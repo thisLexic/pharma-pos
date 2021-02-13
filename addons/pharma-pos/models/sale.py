a08ad8c67c9d588ae9d1cb0e6521c7245885edad
thislexic <benvillabroza1999@gmail.com>
2021-02-13 14:55:48 +0800

    added more fields to Sale UI and model

--- addons/pharma-pos/models/sale.py
+++ addons/pharma-pos/models/sale.py
@@ -4,4 +4,11 @@ class Sale(models.Model):
     _name = 'pharma_pos.sale'
     _description = 'Sell products one at a time'
 
+    def _default_currency_id(self):
+        return self.env['res.currency'].search([('name', '=', 'PHP')], limit=1).id
+
     batch_id = fields.Many2one('pharma_pos.batch', string="Product")
+    currency_id = fields.Many2one('res.currency', string="Currency", default=_default_currency_id)
+    price = fields.Monetary(string="Price")
+    price_total = fields.Monetary(string="Price Total")
+    count = fields.Integer(string="Count", default=1)
