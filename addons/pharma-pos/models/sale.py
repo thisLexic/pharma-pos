f49269122cc23eea853edff9d71023808f3e330c
thislexic <benvillabroza1999@gmail.com>
2021-02-13 20:09:06 +0800

    make price and price_total attribute of Sale model computed

--- addons/pharma-pos/models/sale.py
+++ addons/pharma-pos/models/sale.py
@@ -8,8 +8,24 @@ class Sale(models.Model):
     def _default_currency_id(self):
         return self.env['res.currency'].search([('name', '=', 'PHP')], limit=1).id
 
+    @api.depends('batch_id')
+    def _get_price(self):
+        for record in self:
+            if record.batch_id:
+                record.price = record.batch_id.price_id.price
+            else:
+                record.price = 0.0
+
+    @api.depends('price', 'count')
+    def _get_price_total(self):
+        for record in self:
+            if record.price and record.count:
+                record.price_total = record.price * record.count
+            else:
+                record.price_total = 0.0
+
     batch_id = fields.Many2one('pharma_pos.batch', string="Product")
     currency_id = fields.Many2one('res.currency', string="Currency", default=_default_currency_id)
-    price = fields.Monetary(string="Price")
-    price_total = fields.Monetary(string="Price Total")
+    price = fields.Monetary(string="Price", compute="_get_price", store=True)
+    price_total = fields.Monetary(string="Price Total", compute="_get_price_total", store=True)
     count = fields.Integer(string="Count", default=1)
