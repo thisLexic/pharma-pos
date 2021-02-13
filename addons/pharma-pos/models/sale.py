40bccb4179139416b45d284540386cc4722dfb84
thislexic <benvillabroza1999@gmail.com>
2021-02-13 22:32:22 +0800

    add sub total in pos record

--- addons/pharma-pos/models/sale.py
+++ addons/pharma-pos/models/sale.py
@@ -38,7 +38,21 @@ class Pos(models.Model):
     def _default_currency_id(self):
         return self.env['res.currency'].search([('name', '=', 'PHP')], limit=1).id
 
+    @api.depends('sale_ids.price_total', 'is_senior')
+    def _get_price_total(self):
+        for record in self:
+            if record.sale_ids:
+                sale_total = 0
+                for sale in record.sale_ids:
+                    sale_total += sale.price_total
+                if record.is_senior:
+                    sale_total = sale_total * 0.8 
+                record.price_total = sale_total
+            else:
+                record.price_total = 0.0
+
     sale_ids = fields.One2many('pharma_pos.sale', 'pos_id', string='Sales')
     currency_id = fields.Many2one('res.currency', string="Currency", default=_default_currency_id)
     datetime = fields.Datetime(string="Time", default=fields.Datetime.now())
     is_senior = fields.Boolean(string="Senior?", default=False)
+    price_total = fields.Monetary(string="Sub Total", compute="_get_price_total", store=True)
